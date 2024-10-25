cf70fce07d8f238db8dc5788fb3e7650ec299b04
nilupa786 <129794165+Nilupa-sanjeewani@users.noreply.github.com>
2024-10-25 22:24:58 +0530

    Initial commit

---
+++ Book-Store-MERN/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://lakshan:lakshan12@cluster0.lqkv4.mongodb.net/mern-crud?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
